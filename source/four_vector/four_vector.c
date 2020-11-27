7c5eff181c3968d7abf3bd5622b1aadf3f7e3d95
Thundermoose <tordjarv@gmail.com>
2020-11-27 22:45:21 +0100

    Implemented a four vector
    Despite the fact that I'm doing 3D graphics, I'm going to work in 4D.
    The reason for it is that then I can represent translations in 3D with
    the same matrix machinery that I would use for scaling and rotations.
    
    The four_vector is implemented using avx2 vectors. This means that
    additions, subtractions, elementvise mutliplication and division are all
    single instructions.

---
+++ source/four_vector/four_vector.c
@@ -0,0 +1,57 @@
+#include <four_vector/four_vector.h>
+#include <math.h>
+#include <thundertester/test.h>
+
+float four_vector_norm_3d(four_vector_t vector)
+{
+	four_vector_t vector_square = vector*vector;
+	return sqrt(vector_square[0]+
+		    vector_square[1]+
+		    vector_square[2]);
+}
+
+float four_vector_norm_4d(four_vector_t vector)
+{
+	four_vector_t vector_square = vector*vector;
+	return sqrt(vector_square[0]+
+		    vector_square[1]+
+		    vector_square[2]+
+		    vector_square[3]);
+}
+
+float four_vector_scalar_3d(four_vector_t left_vector,
+			    four_vector_t right_vector)
+{
+	four_vector_t vector_square = left_vector*right_vector;
+	return sqrt(vector_square[0]+
+		    vector_square[1]+
+		    vector_square[2]);
+}
+
+float four_vector_scalar_4d(four_vector_t left_vector,
+			    four_vector_t right_vector)
+{
+	four_vector_t vector_square = left_vector*right_vector;
+	return sqrt(vector_square[0]+
+		    vector_square[1]+
+		    vector_square[2]+
+		    vector_square[3]);
+}
+
+new_test(norm_3d_of_unit_3d_vector,
+	 four_vector_t vector = {0.7821080382182704, 
+	 		       	 0.42726756860548354, 
+			       	 0.4535961214255773,
+			       	 0.0};
+	 assert_that(fabs(four_vector_norm_3d(vector)-1)<1e-12);
+	);
+new_test(norm_4d_of_unit_4d_vector,
+	 four_vector_t vector={0.6407153907146608, 0.35002441322865624, 0.28384457999376717, 0.6216099682706644};
+	 assert_that(fabs(four_vector_norm_4d(vector)-1)<1e-12);
+	);
+
+new_test(scalar_3d_orthogonal,
+	 four_vector_t vector1 = {1,0,0,0};
+	 four_vector_t vector2 = {0,1,0,0};
+	 assert_that(fabs(four_vector_scalar_3d(vector1,vector2))<1e-12);
+	);
---
+++ source/four_vector/four_vector.h
@@ -0,0 +1,15 @@
+#ifndef __FOUR_VECTOR__
+#define __FOUR_VECTOR__
+
+typedef float four_vector_t __attribute__((vector_size(16)));
+
+float four_vector_norm_3d(four_vector_t vector);
+
+float four_vector_norm_4d(four_vector_t vector);
+
+float four_vector_scalar_3d(four_vector_t left_vector,
+			    four_vector_t right_vector);
+
+float four_vector_scalar_4d(four_vector_t left_vector,
+			    four_vector_t right_vector);
+#endif
